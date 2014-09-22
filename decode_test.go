471a994e767a47997fc5c0c6f8efa2c25d0cac8f
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-09-22 15:16:08 -0700

    Improve decoding benchmarks. Fix grammer typos.

--- decode_test.go
+++ decode_test.go
@@ -26,7 +26,7 @@ func testDecode(t *testing.T, tst decodeTest) {
 		t.Fatal(err)
 	}
 
-	// Create an decoder for the audio source
+	// Create a decoder for the audio source
 	decoder, format, err := audio.NewDecoder(file)
 	if err != nil {
 		t.Fatal(err)
@@ -64,17 +64,6 @@ func testDecode(t *testing.T, tst decodeTest) {
 	if samplesTotal != tst.samplesTotal {
 		t.Fatalf("Read %d audio samples, expected %d.\n", samplesTotal, tst.samplesTotal)
 	}
-}
-
-func TestDecodeALaw(t *testing.T) {
-	testDecode(t, decodeTest{
-		file:         "testdata/tune_stereo_44100hz_alaw.wav",
-		samplesTotal: 993530,
-		Config: audio.Config{
-			SampleRate: 44100,
-			Channels:   2,
-		},
-	})
 }
 
 func TestDecodeFloat32(t *testing.T) {
@@ -143,6 +132,17 @@ func TestDecodeInt32(t *testing.T) {
 	})
 }
 
+func TestDecodeALaw(t *testing.T) {
+	testDecode(t, decodeTest{
+		file:         "testdata/tune_stereo_44100hz_alaw.wav",
+		samplesTotal: 993530,
+		Config: audio.Config{
+			SampleRate: 44100,
+			Channels:   2,
+		},
+	})
+}
+
 func TestDecodeMuLaw(t *testing.T) {
 	testDecode(t, decodeTest{
 		file:         "testdata/tune_stereo_44100hz_mulaw.wav",
@@ -154,31 +154,76 @@ func TestDecodeMuLaw(t *testing.T) {
 	})
 }
 
-func BenchmarkInt24(b *testing.B) {
+func benchDecode(b *testing.B, fmt audio.Slice, path string) {
+	// Read the file into memory so we are strictly benchmarking the decoder,
+	// avoiding disk read performance.
 	data, err := ioutil.ReadFile("testdata/tune_stereo_44100hz_int24.wav")
 	if err != nil {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
 
+	// Create a new decoder for the audio source to retrieve the configuration.
+	decoder, _, err := audio.NewDecoder(bytes.NewReader(data))
+	if err != nil {
+		b.Fatal(err)
+	}
+	config := decoder.Config()
+
+	// Create a slice of type fmt large enough to hold 1 second of audio
+	// samples.
+	bufSize := 1 * config.SampleRate * config.Channels
+	buf := fmt.Make(bufSize, bufSize)
+
+	// Decode the entire file b.N times.
 	for i := 0; i < b.N; i++ {
-		// Create an decoder for the audio source
+		// Create a new decoder for the audio source
 		decoder, _, err := audio.NewDecoder(bytes.NewReader(data))
 		if err != nil {
 			b.Fatal(err)
 		}
 
-		// Grab the decoder's configuration
-		config := decoder.Config()
-
-		// Create an buffer that can hold 1 second of audio samples
-		bufSize := 1 * config.SampleRate * config.Channels
-		buf := make(audio.F64Samples, bufSize)
-
-		// Fill the buffer with as many audio samples as we can
-		_, err = decoder.Read(buf)
-		if err != nil {
-			b.Fatal(err)
+		// Read audio samples until there are no more.
+		for {
+			_, err := decoder.Read(buf)
+			if err == audio.EOS {
+				break
+			}
+			if err != nil {
+				b.Fatal(err)
+			}
 		}
 	}
+}
+
+func BenchmarkDecodeFloat32(b *testing.B) {
+	benchDecode(b, audio.F32Samples{}, "testdata/tune_stereo_44100hz_float32.wav")
+}
+
+func BenchmarkDecodeFloat64(b *testing.B) {
+	benchDecode(b, audio.F64Samples{}, "testdata/tune_stereo_44100hz_float64.wav")
+}
+
+func BenchmarkDecodeUint8(b *testing.B) {
+	benchDecode(b, audio.PCM8Samples{}, "testdata/tune_stereo_44100hz_uint8.wav")
+}
+
+func BenchmarkDecodeInt16(b *testing.B) {
+	benchDecode(b, audio.PCM16Samples{}, "testdata/tune_stereo_44100hz_int16.wav")
+}
+
+func BenchmarkDecodeInt24(b *testing.B) {
+	benchDecode(b, audio.PCM32Samples{}, "testdata/tune_stereo_44100hz_int24.wav")
+}
+
+func BenchmarkDecodeInt32(b *testing.B) {
+	benchDecode(b, audio.PCM32Samples{}, "testdata/tune_stereo_44100hz_int32.wav")
+}
+
+func BenchmarkDecodeALaw(b *testing.B) {
+	benchDecode(b, audio.PCM8Samples{}, "testdata/tune_stereo_44100hz_alaw.wav")
+}
+
+func BenchmarkDecodeMuLaw(b *testing.B) {
+	benchDecode(b, audio.MuLawSamples{}, "testdata/tune_stereo_44100hz_mulaw.wav")
 }
