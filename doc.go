6b9fdedd42622c75d9b88f32bf445f79b6f3d3b5
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-10-09 00:12:30 -0700

    Document what types of audio the encoder supports. Fixes #11

--- doc.go
+++ doc.go
@@ -4,8 +4,8 @@
 
 // Package wav decodes and encodes wav audio files.
 //
-// The decoder and encoder are both able to manage all wav audio formats, with
-// any number of channels (except extensible WAV formats), these formats are:
+// The decoder is able to decode all wav audio formats (except extensible WAV
+// formats), with any number of channels. These formats are:
 //
 //  8-bit unsigned PCM
 //  16-bit signed PCM
@@ -16,6 +16,13 @@
 //
 //  μ-law
 //  a-law
+//
+// The encoder is capable of encoding any audio data -- but it currently will
+// convert all data to 16-bit signed PCM on-the-fly before writing to a file.
+//
+// Ultimately this means regardless of what type of audio data you encode, it
+// ends up as a 16-bit WAV file in the end. Future versions of this package
+// will allow the encoder to output the same types as the decoder.
 //
 // Please refer to the WAV specification for in-depth details about its file
 // format:
